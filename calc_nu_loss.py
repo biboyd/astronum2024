ec40afbebcf9a4b04c8803e8e38a2308f85f9d89
biboyd <boyd.brendan@stonybrook.edu>
2024-10-22 17:44:10 -0400

    add plotting of nuloss vs radius

--- calc_nu_loss.py
+++ calc_nu_loss.py
@@ -3,6 +3,8 @@ from calc_optimal_ratio_v_Rconv import (calc_weak_rates, load_data,
                                         calc_mass_arr, plot_results)
 import unyt
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
+cmap = colormaps['Blues']
 
 
 def calc_const_nu_loss(Xne23, Xna23, ecap_nu_loss_arr, bdecay_nu_loss_arr, mass_arr):
@@ -19,6 +21,38 @@ def calc_const_nu_loss(Xne23, Xna23, ecap_nu_loss_arr, bdecay_nu_loss_arr, mass_
 
     return normed_mass * (ecap_losses + bdecay_losses)
 
+
+def plot_nu_losses(rad_arr, Rconv_arr, nu_loss_arr, ratio_arr, sample=4):
+    fig, ax = plt.subplots(1, 1)
+
+    cmap_range = np.max(Rconv_arr) - np.min(Rconv_arr)
+
+    for i, Rconv in enumerate(Rconv_arr):
+        curr_color = cmap((Rconv - np.min(Rconv_arr))/cmap_range)
+        if i % sample:
+            pass
+
+        else:
+            ax.plot(rad_arr, nu_loss_arr[i, :]/(rad_arr[1] - rad_arr[0]), color=curr_color, label=f"Radius: {Rconv:0.0f} km. ratio={ratio_arr[i]:0.1f}")
+
+    # plot real sim data
+    left, right = ax.set_xlim(0., np.max(Rconv_arr))
+
+    sim_nuloss = np.load("nuloss.npy")
+    sim_rad = np.load("radius.npy")/1e5
+    ax.plot(sim_rad, sim_nuloss/(sim_rad[1] - sim_rad[0]), 'k--', label='sim avg. ratio=9ish')
+
+    ax.set_xlim(left, right)
+
+    # set plot params
+    ax.legend(ncols=2)
+    ax.set_xlabel("Stellar Radius (km)")
+    ax.set_ylabel('Total Neutrino Losses (erg/s)')
+    ax.set_facecolor('grey')
+
+    return fig, ax
+
+
 if __name__ == "__main__":
     # load in data
     infile = "init_models/WD_urca_nuc_T5.6e8_rho4.5e9_Mconv1.0.hse.hse.8192"
@@ -33,7 +67,7 @@ if __name__ == "__main__":
     sum_nu_arr = np.empty_like(Rconv_arr)
 
     # save unneeded space as negative
-    full_nu_loss_arr = np.zeros((len(Rconv_arr), len(rad_arr)))-1.
+    full_nu_loss_arr = np.zeros((len(Rconv_arr), len(rad_arr)))
 
     urca_tot = 5e-4
 
@@ -66,7 +100,9 @@ if __name__ == "__main__":
                            sum_nu_arr,
                            ylabel='Total Neutrino Losses (erg/s)')
     fig.savefig("nuloss_vs_rconv.png")
+
+    figall, _ = plot_nu_losses(rad_arr, Rconv_arr, full_nu_loss_arr, ratio_arr)
+    figall.savefig("full_nu_losses.png")
     # save nu loss array data
     np.save("sum_nu_loss.npy", sum_nu_arr)
     np.save("full_nu_loss_v_rad.npy", full_nu_loss_arr)
-
